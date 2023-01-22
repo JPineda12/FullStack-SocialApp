c562ff06eca427be9acbdb5d385bb8a7ee0b7d81
Ubuntu <ubuntu@ip-172-31-43-103.us-east-2.compute.internal>
2023-01-22 22:07:40 +0000

    almost docker

--- u-social/src/main.js
+++ u-social/src/main.js
@@ -7,7 +7,7 @@ import VueAxios from "vue-axios";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
-axios.defaults.baseURL = `http://localhost:3010/api/`
+axios.defaults.baseURL = `http://backend:3010/api/`
 
 createApp(App)
     .use(router)
